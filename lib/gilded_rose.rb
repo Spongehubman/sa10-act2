311e7d4d20b34a80c327648bba08dc23f9624f84
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-02 01:04:14 -0600

    The extra addition made by Sandi Metz in her lecture video uploaded by Confreaks has been added, and now the 'of zero quality' test passed as well.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -62,11 +62,13 @@ class GildedRose
   # Code from Sandi Metz's solution in the Confreaks video,
   # and Jim Weirich provided the original code.
   def normal_tick
-    if @days_remaining > 0
-      @quality -= 1
-    end
-    if @days_remaining <= 0
-      @quality -= 2
+    if @quality != 0
+      if @days_remaining > 0
+        @quality -= 1
+      end
+      if @days_remaining <= 0
+        @quality -= 2
+      end
     end
     @days_remaining -= 1
   end
