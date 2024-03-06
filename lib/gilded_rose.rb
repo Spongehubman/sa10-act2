8fc211465f7ac9cb1537fad22a44e7a3a1258f10
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-06 17:17:01 -0600

    The 'Normal', 'Brie', 'Sulfuras', and 'Backstage' subclasses were all moved inside of the main 'GildedRose' class block. These class blocks are all 'collapsed' in the code editor, just like the 'klass_for(name)' inside of the main 'GildedRose' class block was 'collapsed' in the editor two commits ago.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -36,71 +36,71 @@ class GildedRose
   def days_remaining
     item.days_remaining
   end
-end
 
-
-class Normal
-attr_reader :quality, :days_remaining
-
-def initialize(quality, days_remaining)
-  @quality, @days_remaining = quality, days_remaining
-end
-
-  # Code from Sandi Metz's solution in the Confreaks video,
-  # and Jim Weirich provided the original code.
-  def tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+  class Normal
+    attr_reader :quality, :days_remaining
+    
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    
+    # Code from Sandi Metz's solution in the Confreaks video,
+    # and Jim Weirich provided the original code.
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+    
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+    
+  class Brie
+    attr_reader :quality, :days_remaining
+    
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    
+    # Code from Sandi Metz's solution in the Confreaks video,
+    # and Jim Weirich provided the original code.
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+    
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+    
+  class Sulfuras
+    attr_reader :quality, :days_remaining
+    
+    def initialize
+      @quality, @days_remaining = quality, days_remaining
+    end
+    
+    def tick
+    end
+  end
+    
+  class Backstage
+    attr_reader :quality, :days_remaining
+    
+    def initialize
+      @quality, @days_remaining = quality, days_remaining
+    end
+    
+    def tick
+      @days_remaining -= 1
+      return              if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+    
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+    end
   end
 end
 
 
-class Brie
-  attr_reader :quality, :days_remaining
-
-  def initialize(quality, days_remaining)
-    @quality, @days_remaining = quality, days_remaining
-  end
-
-  # Code from Sandi Metz's solution in the Confreaks video,
-  # and Jim Weirich provided the original code.
-  def tick
-    @days_remaining -= 1
-    return if @quality >= 50
-
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
-  end
-end
-
-class Sulfuras
-  attr_reader :quality, :days_remaining
-
-  def initialize
-    @quality, @days_remaining = quality, days_remaining
-  end
-
-  def tick
-  end
-end
-
-class Backstage
-  attr_reader :quality, :days_remaining
-
-  def initialize
-    @quality, @days_remaining = quality, days_remaining
-  end
-
-  def tick
-    @days_remaining -= 1
-    return              if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
-  end
-end
