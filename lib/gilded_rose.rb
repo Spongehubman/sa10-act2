522b7c94bd7451c5f9b3eadff1addb62da04a02f
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-06 21:00:30 -0600

    A new hash for discerning what kind of 'Item' class instance it is has been added above the 'GildedRose' class's 'self.new' method. In the aforementioned 'self.new' method, the 'new(quality, days_remaining)' code was made separate from its 'klass_for(name).' code, which has been replaced with an new statement for taking advantage of the hash to create the correct kind of 'Item' class instance. Finally, the entire 'self.klass_for(name)' method containing the 'case' code has been completely removed. Also, the 'Normal < Item' and 'Brie < Item' sub-classes, and all code inside of them, have been re-added. My bad.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,31 +1,41 @@
 module GildedRose
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item'                               => Normal,
+    'Aged Brie'                                 => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage}
+
   def self.new(name:, quality:, days_remaining:)
-    klass_for(name).new(quality, days_remaining)
-  end
-
-
-  def self.klass_for(name)
-
-    # Code from Sandi Metz's solution in the Confreaks video,
-    # and Jim Weirich provided the original code.
-    case name
-    when 'Normal Item'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Item
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
-
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
+      new(quality, days_remaining)
   end
 
   class Item
     def tick
     end
   end
+
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
     
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+    
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
   class Backstage < Item
     def tick
       @days_remaining -= 1
